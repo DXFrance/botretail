eebce4e230234c374c4033813a54a6b260c004c4
Etienne Margraff <etmargra@microsoft.com>
2016-08-22 15:34:18 +0200

    copy data to bin folder during gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     typescript = require('gulp-typescript');
 
-gulp.task('default', function(){
+gulp.task('default', ['copy-data'], function(){
   return gulp.src(['**/*.ts', '!node_modules/**/*.*'])
         .pipe(typescript({
             noImplicitAny: true
         }))
         .pipe(gulp.dest('bin'));
+});
+
+gulp.task('copy-data', function(){
+    return gulp.src(['data/**/*'], {base:"."})
+    .pipe(gulp.dest("bin"));
 });
