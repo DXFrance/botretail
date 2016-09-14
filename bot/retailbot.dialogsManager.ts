d867b0ee2ab44e28f5891991135e4d1e37846208
Etienne Margraff <etmargra@microsoft.com>
2016-09-14 13:14:48 -0700

    test

--- bot/retailbot.dialogsManager.ts
+++ bot/retailbot.dialogsManager.ts
@@ -276,7 +276,7 @@ export namespace RETAILBOT {
                                             .tap(builder.CardAction.showImage(session, "http://www.timstanleyphoto.com/HDR/2012/i-GrS2b37/0/L/MicrosoftStore-L.jpg")),
                                     ])
                                     .buttons([
-                                        builder.CardAction.openUrl(session, "http://bing.com/maps/default.aspx?rtp=adr." + "39 quai du president roosevelt 92130 issy les moulineaux" + "~adr." + "21 Rue Pasteur, Le Kremlin-Bicêtre, IdF 94270, France" + "&rtop=0~1~0", "Bing Direction"),
+                                        builder.CardAction.openUrl(session, "http://bing.com/maps/default.aspx?rtp=adr." + "39%20quai%20du%20president%20roosevelt%2092130%20issy%20les%20moulineaux" + "~adr." + "21%20Rue%20Pasteur%20%20Le%20Kremlin-Bicêtre%20%20IdF%2094270%20France" + "&rtop=0~1~0", "Bing Direction"),
                                         builder.CardAction.imBack(session, "Let's go !", "Go")
                                     ])
                             ]);
